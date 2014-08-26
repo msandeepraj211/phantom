f9756d680d0e8b472decb9c8585f3ccc677b4147
msandeepraj211 <msandeepraj211@gmail.com>
2014-08-26 11:54:45 +0530

    backup before changeing top bar

--- html site/initializr/js/directory.js
+++ html site/initializr/js/directory.js
@@ -1,9 +1,10 @@
 /**
  * Created by raj on 21/8/14.
  */
-$('.search-box input').width($('.top-bar div div div div').width()-($('#top-bar-logo').height()/2+15));
+$('.search-box input').width($('.top-bar div div div div').width()-($('#top-bar-logo').height()/2+13));
 $('.search-box input').height($('#top-bar-logo').height()/2.5);
 $('.search-box img').width($('#top-bar-logo').height()/2);
 $('.search-box').css('margin-top',$('#top-bar-logo').height()/6 +'px');
 $('.social').css('margin-top',$('#top-bar-logo').height()/6 +'px');
 $('#anime-dir').css('margin-top',$('.top-bar').height()*1.4 +'px');
+$('#sort-bar').sticky({topSpacing:$('.top-bar').height()})
--- recent.js
+++ recent.js
@@ -3,49 +3,34 @@
  */
 
 var fs = require('fs');
-var content = fs.read ('animeEpisode.json');
-console.log(JSON.stringify(JSON.parse(content)[1][0].title));
-videolinks=JSON.parse(content);
-links=[];
 
-function pages(k) {
+function pages() {
         var page = new WebPage();
-        page.open('http://www.gogoanime.com/', function (status) {
+        page.open('http://www.gogoanime.com', function (status) {
             console.log('opened gogoanime :++++ ', status);
-            if (status==fail){
+            if (status=="fail"){
                 page.close();
-                pages(k);
+                pages();
             }
-            if (status == success) {
+            if (status == "success") {
 
                 page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', function () {
-                    console.log('jq included')
-                    var data = page.evaluate(function (data) {
-                        var tempdata=[];
+                    console.log('jq included');
+
+                    var data = page.evaluate(function () {
+                        var tempdata={};
+                        tempdata.links=[];
                         for (var i = 0; i <$('.post div:eq(1) table tbody tr td:eq(0) ul').length; i = i + 1) {
-                            data.links.push($('.post div:eq(1) table tbody tr td:eq(0) ul li a').attr('href'));
+                            if( $('.post div:eq(1) table tbody tr td:eq(0) ul:eq('+i+') li font').html()=="(Sub)"){
+                                tempdata.links.push($('.post div:eq(1) table tbody tr td:eq(0) ul:eq('+i+') li a').attr('href'));
                         }
-                        return JSON.stringify(data);
+                        }
+                        return JSON.stringify(tempdata);
                     });
-                    links[k][m] = JSON.parse(data);
                     console.log(data);
-                    if (m < links[k].length - 1) {
-                        page.close();
-                        console.log('next episoide called');
-                        pages(k, m + 1);
-                    }
-                    ;
-                    if (m == links[k].length - 1) {
-                        page.close();
-                        console.log('next anime called');
-                        var path = 'links.json';
-                        fs.write(path, links[k], 'w');
-                        pages(k + 1, 1);
-                    }
-                    if (k == links.length - 1) {
-                        var path = 'links.json';
-                        fs.write(path, links, 'w');
-                    }
+                    var path = 'recent.txt';
+                    fs.write(path, data, 'w');
+
                 });
             }
         });
